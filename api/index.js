66109b1135c14d23759e99f36673203351187a28
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2023-01-31 20:35:33 +0530

    adding links and setting up vercel

---
+++ api/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const path = require("path");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  // set cache control header
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  res.render("index");
+});
+
+app.post("/contact", (req, res) => {
+  // setup nodemailer
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: EMAIL,
+      pass: PASSWORD,
+    },
+  });
+
+  let email1 = transporter.sendMail({
+    from: EMAIL,
+    to: EMAIL,
+    subject: "Contact Form",
+    html: `
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `,
+  });
+
+  let email2 = transporter.sendMail({
+    from: EMAIL,
+    to: req.body.email,
+    subject: "Contact Form",
+    html: `
+      <h3>Thank you for contacting us</h3>
+      <p>We will get back to you soon</p>
+    `,
+  });
+
+  Promise.all([email1, email2])
+    .then(() => {
+      res.json({ msg: "Emails sent" });
+    })
+    .catch((err) => {
+      res.json({ msg: "Emails not sent" });
+    });
+});
+
+module.exports = app;
